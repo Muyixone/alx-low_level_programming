f2387136b47f7de6085fc08f5d3e41c33bf40e6c
muyixone <muyixone@yahoo.com>
2022-03-12 08:16:37 -0800

    Using printf function in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: returns a positive value when successful
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
