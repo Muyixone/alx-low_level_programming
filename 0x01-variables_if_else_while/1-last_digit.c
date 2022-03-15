defdba298bd4d274efc5cdee776870ab0abc4cea
muyixone <muyixone@yahoo.com>
2022-03-14 21:18:24 -0700

    If..Else statement with a logical & operator

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	else if (n == 0)
 	{
 		scanf("%d, %d", &n, &s);
-		printf("Last digit of %d is 0 and is 0", n, s);
+		printf("Last digit of %d is %d and is 0", n, s);
 	}
 	else if ((n < 6) && (n < 0))
 	{
