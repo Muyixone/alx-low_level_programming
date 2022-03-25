749dc5335b014f54e62139806c1e29e19f79af79
muyixone <muyixone@yahoo.com>
2022-03-24 19:51:25 -0700

    Function that concatenates strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,35 +1,46 @@
 #include "main.h"
 
 /**
- *_strncpy - A function that copies src into dest
+ *_strncat - function that concatenates two strings
+ *it will use at most n bytes from src. src does not need to be null terminated
+ *
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: number of bytes to be concatenated
+ *
+ *Return: pointer to destination string
+ *_strncat - concatinate 2 strings
  *@dest: first string
  *@src: second string
  *@n: the number of bytes to use from src
- *
- *Return: string.
- */
+*/
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, m = 0;
+	int length, j;
+/* j is a counter for  n bytes of src to be concatenated */
+/* length = length of destination string */
+	int i = 0, k = 0;
 
-	while (n > m)
+	length = 0;
+	while (dest[length] != '\0')
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[k] != '\0' && n > k)
 	{
-		if (src[m] == '\0')
-		{
-			for (; m < n; m++)
-			{
-				dest[i] = '\0';
-				i++;
-			}
-		}
-		else
-		{
-			dest[i] = src[m];
-			m++;
-			i++;
-		}
+		length++;
+		dest[i] = src[k];
+		k++;
+		i++;
 	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	if (n > 0)
+	{
+		dest[length] = src[j];
+		dest[i] = '\0';
+	}
+	dest[length] = '\0';
 
 	return (dest);
 }
