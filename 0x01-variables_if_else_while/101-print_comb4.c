c3aee7630a03e39c4b15666747013665e08a2581
muyixone <muyixone@yahoo.com>
2022-04-20 17:33:03 -0700

    Program that prints 3 possible combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Prints different combinations of three digits
+ * Return: Always 0 Success
+*/
+int main(void)
+{
+	int numb1, numb2, numb3;
+
+	numb1 = '0';
+
+	while (numb1 <= '7')
+	{
+		numb2 = '0';
+
+		while (numb2 <= '8')
+		{
+			numb3 = '2';
+
+			while (numb3 <= '9')
+			{
+				if (numb1 != numb2 && numb2 != numb3 && numb3 != numb1)
+				{
+					putchar(numb1);
+					putchar(numb2);
+					putchar(numb3);
+					if (numb1 != '7' || numb2 != '8' || numb3 != '9' ||
+					(numb1 != '7' && numb2 != '8' && numb3 != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				numb3++;
+			}
+			numb2++;
+		}
+		numb1++;
+	}
+	putchar('\n');
+	return (0);
+}
