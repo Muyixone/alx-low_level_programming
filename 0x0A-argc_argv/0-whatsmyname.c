bf367c0b0e5a106a9d912b8527888cde7eae18e7
Osamuyi <osamuyia1@gmail.com>
2023-11-19 22:05:08 +0100

    Removed the main.h header file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 /**
   * main - The main function that accepts commnand line arguments.
   * @argc: Integer parameter that counts the number of arguments
@@ -13,6 +12,6 @@
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf(" %s \n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
