0e9380a3da62d4c22b96d966c55762cd3744d7a8
Osamuyi <osamuyia1@gmail.com>
2023-11-19 21:46:32 +0100

    Created a function that prints the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - The main function that accepts commnand line arguments.
+  * @argc: Integer parameter that counts the number of arguments
+  * past in the command line.
+  * @argv: A string of character array past
+  * to the command line.
+  *
+  * Return: 0 success.
+*/
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf(" %s \n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+##Arguments to main function
+
+file 0-whatsmyname.c: contains a function that prints the name of the program. This function accepts argument past from the command line.
