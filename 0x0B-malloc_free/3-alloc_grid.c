4aedbd16974cb748c1b700abfcce53897507cb2d
Osamuyi <osamuyia1@gmail.com>
2023-11-22 20:48:55 +0100

    fixed output error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,8 +19,10 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 	ptr = malloc(width * sizeof(*ptr));
+	if (ptr == NULL)
+		return (NULL);
 	arr = malloc(width * height * sizeof(int));
-	if (ptr == NULL || arr == NULL)
+	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
