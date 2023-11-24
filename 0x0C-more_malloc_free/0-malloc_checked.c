04a382ceab665400b033b85bac9e3e275960f665
Osamuyi <osamuyia1@gmail.com>
2023-11-24 02:50:09 +0100

    Created a function that returns a pointer to the allocated memory location

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * malloc_checked- Stores int to allocated point in memory.
+  * @b: Integer paramter.
+  *
+  * Return: Pointer to memory location
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *memory;
+	unsigned int i;
+
+	memory = malloc(sizeof(*memory) * b);
+	if (memory == NULL)
+		exit(98);
+
+	for (i = 0; i < b; i++)
+		memory[i] = b;
+	return (memory);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+##MORE MALLOC_FREE
+
+File 0-malloc_checked.c: Is a program that returns a pointer to an allocated space in memory.
