b1241d7bb41ed270c9a4841ca75d2cc3c3771759
Osamuyi <osamuyia1@gmail.com>
2023-11-24 03:45:50 +0100

    Fixed malloc error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -13,13 +13,10 @@ void *malloc_checked(unsigned int b)
 	int *memory;
 	unsigned int i;
 
-	if (b == 0)
-		exit(98);
-
-	memory = malloc(sizeof(int) * b);
+	memory = (int *) malloc(sizeof(int) * b);
 	if (memory == NULL)
 		exit(98);
 	for (i = 0; i < b; i++)
-		memory[i] = b - i;
+		memory[i] = b;
 	return (memory);
 }
