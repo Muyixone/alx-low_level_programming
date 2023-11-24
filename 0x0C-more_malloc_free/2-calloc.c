97b895048824836105b8708de10c667a88e67573
Osamuyi <osamuyia1@gmail.com>
2023-11-24 22:17:20 +0100

    Created a function that allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
   * _calloc- Allocates memory for an array/
-  * @nmemb: First parameter.
+  * @nmemb: First parameter containing array of integers.
   * @size: Size of the array.
   *
   * Return: A pointer to the memory location.
@@ -16,12 +16,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	arr = (int *)malloc(sizeof(int) * size);
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		arr[i] = nmemb;
+		arr[i] = 0;
 	return (arr);
 }
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,5 @@
 ##MORE MALLOC_FREE
 
 File 0-malloc_checked.c: Is a program that returns a pointer to an allocated space in memory.
+
+File 2-calloc.c: Is a program that allocates memory for an array. It initializes memory to zero.
