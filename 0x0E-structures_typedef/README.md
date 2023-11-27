0fc7ab1015605f3135883865c481bdaac4925265
Osamuyi <osamuyia1@gmail.com>
2023-11-27 23:48:58 +0100

    Created a struct data for a dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+## Structures and typedef in C
+
+This directory contains solutions to tasks on struct and typdef in C language.
+
+File dog.h contains a header file that defines a struct datatype.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef pet_dog
+#define pet_dog
+/**
+  * struct dog - A structore for a poppy
+  * @name: The name of the dog
+  * @age: The dog's age
+  * @owner: The name of the dog's owner
+  *
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
