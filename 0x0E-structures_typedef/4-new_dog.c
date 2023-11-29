004083c2a78157452bd2ebb30a2458ef19ba3562
Osamuyi <osamuyia1@gmail.com>
2023-11-29 04:55:17 +0100

    Wrote a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,65 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * new_dog - Creates a new dog.
+  * @name: Name of the dog as a parameter.
+  * @age: Age of the dog as a param.
+  * @owner: The dog's owner.
+  *
+  * Return: A pointer.
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->name = _strdup(name);
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->owner = _strdup(owner);
+	if (ptr->owner == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->age = age;
+
+	return (ptr);
+}
+
+/**
+  * _strdup - Returns a pointer to a newly allocated space in memory.
+  * @str: String paramter.
+  * Return: Returns a pointer to sring in memory
+*/
+
+char *_strdup(char *str)
+{
+	size_t i;
+	char *p;
+	size_t count;
+
+	if (str == NULL)
+		return (NULL);
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	p = malloc(sizeof(*str) * count + 1);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+		p[i] = str[i];
+
+	p[i] = '\0';
+	return (p);
+}
+
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -7,3 +7,5 @@ File dog.h contains a header file that defines a struct datatype.
 File 1-init_dog.h contains a function that initializes a variable of type struct
 
 File 2-print_dog.c contains a function that prints a struct dog
+
+File 4-new_dog.c contains a function that creates copy of a new dog
