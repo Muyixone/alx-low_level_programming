7413a5e5fcc4ed3b17aa0c8fe938441f6d664185
Osamuyi <osamuyia1@gmail.com>
2023-11-29 01:43:13 +0100

    Created a function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * print_dog - Prints a struct dog
+  * @d: Parameter to the struct dog
+  * Return: Void is returned.
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else if (!d->age)
+			printf("Age: (nil)\n");
+		else if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+	else
+	{
+	}
+
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -5,3 +5,5 @@ This directory contains solutions to tasks on struct and typdef in C language.
 File dog.h contains a header file that defines a struct datatype.
 
 File 1-init_dog.h contains a function that initializes a variable of type struct
+
+File 2-print_dog.c contains a function that prints a struct dog
