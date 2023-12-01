fc51a8cc473ed99f5e132016bdc0157535a7f478
Osamuyi <osamuyia1@gmail.com>
2023-12-01 03:50:18 +0100

    Fixed compilation error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,15 +14,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (n == 0)
-		return;
-	if (separator == NULL)
-		return;
-
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", va_arg(ap, int), (i == n - 1) ? "" : ", ");
+		if (n  == 0 && separator == NULL)
+			return;
+		else if (i)
+			printf("%s", separator);
+		printf("%d", va_arg(ap, int));
 	}
 	va_end(ap);
 	printf("\n");
