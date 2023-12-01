1a41554f46aa605c411c8de67fd918297691ea9f
Osamuyi <osamuyia1@gmail.com>
2023-12-01 02:12:16 +0100

    Fixed linter error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum, i;
 	va_list ap;
-	
-	if ( n == 0)
-		return 0;	
+
+	if (n == 0)
+		return (0);
 	va_start(ap, n);
 	sum = 0;
-
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
