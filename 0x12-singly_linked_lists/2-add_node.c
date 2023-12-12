e8fbf3496be2b249c19255ad43dcdaf70f1b49cb
Osamuyi <osamuyia1@gmail.com>
2023-12-12 01:14:48 +0100

    Freed all allocated memory block

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,6 +22,8 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->len = count;
 	new_node->next = *head;
 	*head = new_node;
-
+	
+	free(new_node->str);
+	free(new_node);
 	return (*head);
 }
