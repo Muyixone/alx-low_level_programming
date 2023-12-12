617e7fcbed9ca1c87f966c406198afcf95952101
Osamuyi <osamuyia1@gmail.com>
2023-12-12 02:36:19 +0100

    Fixed memory leak when malloc fails

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,15 +14,13 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	if (new_node->str == NULL || new_node == NULL)
 	{
 		free(new_node->str);
+		free(new_node);
 		return (NULL);
 	}
 	while (new_node->str[count] != '\0')
@@ -30,6 +28,5 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->len = count;
 	new_node->next = *head;
 	*head = new_node;
-	
 	return (*head);
 }
