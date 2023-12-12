d3ef9d27de9314de4bcf86792658f05229b35bc8
Osamuyi <osamuyia1@gmail.com>
2023-12-12 01:07:42 +0100

    Created a function that adds a new node to the beginning of a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+  * add_node - Function pointer to a struct.
+  * @head: Pointer parameter to the first node in a linke list.
+  * @str: String paramter. A variable in the struct list_t.
+  *
+  * Return: 1 on success, 0 otherwise.
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	size_t count = 0;
+
+	new_node = (list_t *) malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	while (new_node->str[count] != '\0')
+		count++;
+	new_node->len = count;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -2,3 +2,4 @@
 
 File: 0-print_list.c contains a function that prints the elements of a list.
 File: 1-list_len.c contains a function that returns the number of elements in a list.
+File: 2-add_node.c contains a function that adds a new node to the beginning of the linked list.
